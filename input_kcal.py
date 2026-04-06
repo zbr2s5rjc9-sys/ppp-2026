ab0bde73a0fe653da7ee5c75a6cd152a68a111c0
rmsdud614 <zbr2s5rjc9@privaterelay.appleid.com>
2026-04-06 20:24:23 +0900

    Delete input_kcal.py

--- input_kcal.py
+++
@@ -1,9 +0,0 @@
-orange = float(input("한라봉의 그람(g)수를 입력하세요 : "))
-strawberry = float(input("딸기의 그람(g)수를 입력하세요 : "))
-banana = float(input("바나나의 그람(g)수를 입력하세요 : "))
-
-orange_kcal = orange / 2
-strawberry_kcal = strawberry / (100 / 34)
-banana_kcal = banana / (100/77)
-
-print(orange_kcal, strawberry_kcal, banana_kcal)
