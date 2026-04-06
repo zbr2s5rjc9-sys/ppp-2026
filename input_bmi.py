4af388aa33fd0b7079478f75a1356065781dcc95
rmsdud614 <zbr2s5rjc9@privaterelay.appleid.com>
2026-04-06 13:52:11 +0900

    Delete input_bmi.py

--- input_bmi.py
+++
@@ -1,9 +0,0 @@
-weight = float(input("몸무게(kg)를 입력해보세요 : "))
-height = float(input("키(cm)를 입력해보세요 : "))
-
-height_m = height / 100
-BMI = weight / (height_m **2)
-
-print(BMI)
-
-print(f"나의 BMI 지수는 {BMI}다!")
